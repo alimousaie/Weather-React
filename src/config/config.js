6e8b678559df4f4b2d21777bd74c0dda50a81370
Ali Mousaie <a.mousaie@gmail.com>
2021-12-06 04:54:17 +0100

    config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const userConfig = {
 	weather_api_url: 'http://api.weatherstack.com/current',
-	weather_api_key: '768410514ffaabe3146e7685082e6201',
+	weather_api_key: 'Your Api Key',
 	cities_api_url: 'https://countriesnow.space/api/v0.1/countries/capital',
 };
